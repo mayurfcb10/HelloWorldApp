10ebc21e310759d303cffc8fbc91ff6eb31618ad
Mayur <mayurzope555@gmail.com>
2020-11-27 20:56:23 +0530

    [Mayur]Added: Display hello from bridgelabz with user name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      title: "Hello from Bridgelabz !!"
+      userName: ""
     };
   }
   // onClick function
@@ -15,15 +15,26 @@ class App extends React.Component {
     console.log("save button is clicked ", { event });
     window.open(this.url, "_blank");
   };
+  //onChange Event function
+  onNameChangeHandler = (event) => {
+    console.log("value is ", event.target.value);
+    // see the title using setState Method
+    this.setState({ userName: event.target.value });
+  };
   render() {
     return (
       <div>
-        <h1>{this.state.title}</h1>
-        <img
-          src={logo}
-          onClick={this.onClick}
-          alt="The BridgeLabz logo: A Bridge to Employement through lab works"
-        />
+        <div>
+          <h1>Hello {this.state.userName} from BridgeLabz</h1>
+          <img
+            src={logo}
+            onClick={this.onClick}
+            alt="The BridgeLabz logo: A Bridge to Employement through lab works"
+          />
+        </div>
+        <div className="text-box">
+          <input onChange={this.onNameChangeHandler} />
+        </div>
       </div>
     );
   }
