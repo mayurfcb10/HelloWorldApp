1e767269041b23be456abd7c72a142f854b2aa74
Mayur <mayurzope555@gmail.com>
2020-11-27 20:32:50 +0530

    [Mayur]Added: Event binding technique to bind the click event on the bridgelabz logo

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import "./styles.css";
 import logo from "../src/assets/logo.jpg";
 
 class App extends React.Component {
+  url = "https://www.bridgelabz.com/";
   constructor() {
     super();
     this.state = {
-      title: "HelloWorld Demo App  from Bridgelabz !!"
+      title: "Hello from Bridgelabz !!"
     };
   }
+  // onClick function
+  onclick = (event) => {
+    console.log("save button is clicked ", { event });
+    window.open(this.url, "_blank");
+  };
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
         <img
           src={logo}
+          onClick={this.onClick}
           alt="The BridgeLabz logo: A Bridge to Employement through lab works"
         />
       </div>
